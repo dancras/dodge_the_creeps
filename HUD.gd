39566452164f7291585b2bad999c5ff621c6be19
Daniel Howlett <dan@dancras.co.uk>
2020-05-28 21:44:05 -0600

    Add a leaderboard view
    Alternates with game title message on timer. No leaderboard functionality has
    been implemented so far.

--- HUD.gd
+++ HUD.gd
@@ -2,20 +2,6 @@ extends CanvasLayer
 
 signal start_game
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#    pass
-
 func show_message(text):
     $Message.text = text
     $Message.show()
@@ -28,6 +14,7 @@ func show_game_over():
 
     $Message.text = "Dodge the\nCreeps!"
     $Message.show()
+    $LeaderboardTimer.start()
 
     yield(get_tree().create_timer(1), "timeout")
     $StartButton.show()
@@ -40,6 +27,14 @@ func _on_StartButton_pressed():
     $StartButton.hide()
     emit_signal("start_game")
 
+    $Leaderboard.hide()
+    $LeaderboardTimer.stop()
+
 
 func _on_MessageTimer_timeout():
     $Message.hide()
+
+
+func _on_LeaderboardTimer_timeout():
+    $Message.visible = !$Message.visible
+    $Leaderboard.visible = !$Leaderboard.visible
